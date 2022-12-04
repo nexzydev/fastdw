fe29c5eb7999aae0bfb997a9344f2fd8edc111a4
m7codeex <119813958+m7codeex@users.noreply.github.com>
2022-12-04 16:42:44 -0300

    Add files via upload

---
+++ s.py
@@ -0,0 +1,20 @@
+import socket, requests
+
+s = socket.socket()         
+ 
+s.bind(('localhost', 8888 ))
+s.listen(0)                 
+ 
+while True:
+    client, addr = s.accept()
+
+    while True:
+        content = client.recv(2042)
+        if len(content) == 0:
+           break
+        else:
+            print(f"enviando o arquivo {content.decode()}")
+            req = requests.get(content.decode("utf-8"))
+            client.send(req.content)
+            print("Arquivo enviado, fim da conexao")
+    client.close()
